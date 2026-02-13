fe09755e9ef26d3992ad578b218a992b4d7f6c73
klabir <45362306+klabir@users.noreply.github.com>
2026-02-13 21:55:03 +0100

    fix(sensor): use integration domain for hass.data lookup

--- custom_components/wnsm/sensor.py
+++ custom_components/wnsm/sensor.py
@@ -11,25 +11,26 @@ from homeassistant import core, config_entries
 from homeassistant.components.sensor import (
     PLATFORM_SCHEMA
 )
-from homeassistant.const import (
-    CONF_USERNAME,
-    CONF_PASSWORD,
-    CONF_DEVICE_ID
+from homeassistant.const import CONF_SCAN_INTERVAL
+from .const import (
+    CONF_ENABLE_DAY_STATISTICS_IMPORT,
+    CONF_ZAEHLPUNKTE,
+    DEFAULT_SCAN_INTERVAL_MINUTES,
+    DOMAIN,
 )
-from homeassistant.core import DOMAIN
 from homeassistant.helpers.typing import (
     ConfigType,
     DiscoveryInfoType,
 )
-from .const import CONF_ZAEHLPUNKTE
+from .day_sensor import WNSMDailySensor
 from .wnsm_sensor import WNSMSensor
 # Time between updating data from Wiener Netze
-SCAN_INTERVAL = timedelta(minutes=60 * 6)
+SCAN_INTERVAL = timedelta(minutes=DEFAULT_SCAN_INTERVAL_MINUTES)
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
-        vol.Required(CONF_USERNAME): cv.string,
-        vol.Required(CONF_PASSWORD): cv.string,
-        vol.Required(CONF_DEVICE_ID): cv.string,
+        vol.Required("username"): cv.string,
+        vol.Required("password"): cv.string,
+        vol.Required("device_id"): cv.string,
     }
 )
 
@@ -41,10 +42,24 @@ async def async_setup_entry(
 ):
     """Setup sensors from a config entry created in the integrations UI."""
     config = hass.data[DOMAIN][config_entry.entry_id]
+    scan_interval_minutes = config.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL_MINUTES)
+    global SCAN_INTERVAL
+    SCAN_INTERVAL = timedelta(minutes=scan_interval_minutes)
     wnsm_sensors = [
-        WNSMSensor(config[CONF_USERNAME], config[CONF_PASSWORD], zp["zaehlpunktnummer"])
+        WNSMSensor(config["username"], config["password"], zp["zaehlpunktnummer"])
         for zp in config[CONF_ZAEHLPUNKTE]
     ]
+    wnsm_sensors.extend(
+        [
+            WNSMDailySensor(
+                config["username"],
+                config["password"],
+                zp["zaehlpunktnummer"],
+                config.get(CONF_ENABLE_DAY_STATISTICS_IMPORT, False),
+            )
+            for zp in config[CONF_ZAEHLPUNKTE]
+        ]
+    )
     async_add_entities(wnsm_sensors, update_before_add=True)
 
 
@@ -57,5 +72,11 @@ async def async_setup_platform(
     ] = None,  # pylint: disable=unused-argument
 ) -> None:
     """Set up the sensor platform by adding it into configuration.yaml"""
-    wnsm_sensor = WNSMSensor(config[CONF_USERNAME], config[CONF_PASSWORD], config[CONF_DEVICE_ID])
-    async_add_entities([wnsm_sensor], update_before_add=True)
+    wnsm_sensor = WNSMSensor(config["username"], config["password"], config["device_id"])
+    wnsm_daily_sensor = WNSMDailySensor(
+        config["username"],
+        config["password"],
+        config["device_id"],
+        config.get(CONF_ENABLE_DAY_STATISTICS_IMPORT, False),
+    )
+    async_add_entities([wnsm_sensor, wnsm_daily_sensor], update_before_add=True)
